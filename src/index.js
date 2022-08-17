26f571f59d697986dfee5647a6bd96827ba15732
abhi587 <abhishek.pawan14@gmail.com>
2022-08-17 20:50:08 +0530

    17th Aug assigment

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 
-mongoose.connect("mongodb+srv://functionup-cohort:G0Loxqc9wFEGyEeJ@cluster0.rzotr.mongodb.net/Pritesh8769811-DB?retryWrites=true&w=majority", {
+mongoose.connect("mongodb+srv://RaviKumarSharma:i6tpVmiNCvIQSjH6@cluster0.pnzdn4a.mongodb.net/abhi123-DB", {
     useNewUrlParser: true
 })
 .then( () => console.log("MongoDb is connected"))
