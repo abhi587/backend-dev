797c75315d80dd29255fe9679f56f188f4d5b7ef
abhi587 <abhishek.pawan14@gmail.com>
2022-08-24 19:14:14 +0530

    24thAug assigment

--- src/index.js
+++ src/index.js
@@ -3,9 +3,13 @@ const bodyParser = require('body-parser');
 const route = require('./routes/route.js');
 const { default: mongoose } = require('mongoose');
 const app = express();
+const GlobalMiddleware = require('./middlewares/globalMiddleware')
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+
+
+app.use (GlobalMiddleware.globalMiddleware);
 
 
 mongoose.connect("mongodb+srv://functionup-cohort:G0Loxqc9wFEGyEeJ@cluster0.rzotr.mongodb.net/Pritesh8769811-DB?retryWrites=true&w=majority", {
@@ -14,12 +18,6 @@ mongoose.connect("mongodb+srv://functionup-cohort:G0Loxqc9wFEGyEeJ@cluster0.rzot
 .then( () => console.log("MongoDb is connected"))
 .catch ( err => console.log(err) )
 
-app.use (
-    function (req, res, next) {
-        console.log ("inside GLOBAL MW");
-        next();
-  }
-  );
 
 app.use('/', route);
 
