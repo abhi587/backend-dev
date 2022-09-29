dbea8c73333d79ff44f990a9dad1c6560e55c8f8
abhi587 <abhishek.pawan14@gmail.com>
2022-09-29 18:04:27 +0530

    day1

--- src/routes/route.js
+++ src/routes/route.js
@@ -4,12 +4,19 @@ const UrlController = require('../controllers/urlController')
 
 // test API
 router.get('/test', function(req, res){
-    res.send({status : true, message : "Test API working fine"})
+    res
+        .status(200)
+        .send({status : true, message : "Test API working fine"})
 })
 
 // API for url shortening 
 router.post('/url/shorten', UrlController.urlShortener )
 
 
+router.all("/**", function (req,res){
+    res
+        .status(404)
+        .send({status:false, message:"the api you requested is not available"});
+})
 
 module.exports = router
